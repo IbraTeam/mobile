732b018723f0b154550ed22dfce678fbd74f3d8c
pupov <pupov435@gmail.com>
2024-02-17 17:03:31 +0700

    Добавил навгацию

--- app/src/main/java/com/ibra/keytrackerapp/common/navigation/KeyTrackerNavigation.kt
+++ app/src/main/java/com/ibra/keytrackerapp/common/navigation/KeyTrackerNavigation.kt
@@ -5,7 +5,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.ibra.keytrackerapp.keytrack.presentation.KeyTrackerScreen
+import com.ibra.keytrackerapp.login.LoginScreen
+import com.ibra.keytrackerapp.sign_in_sign_up.SignInSignUpScreen
 
 @Composable
 fun KeyTrackerNavigation(
@@ -13,11 +14,18 @@ fun KeyTrackerNavigation(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Screen.KeyTracker.name
+        startDestination = Screen.SignInSignUpScreen.name
     ) {
+        composable(Screen.SignInSignUpScreen.name) {
+            SignInSignUpScreen(navController = navController)
+        }
         composable(Screen.KeyTracker.name) {
             KeyTrackerScreen(navController = navController)
+        }
+        composable(Screen.Login.name) {
+            LoginScreen(navController = navController)
         }
     }
 
 }
+
