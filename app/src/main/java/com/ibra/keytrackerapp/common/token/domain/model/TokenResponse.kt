18a9bb1a22304dcd9516e50b8cf00a5d1bf68a51
KovkinAleksei <akovkin6@gmail.com>
2024-02-21 13:47:25 +0700

    Исправлены пути в запросах

--- app/src/main/java/com/ibra/keytrackerapp/common/token/domain/model/TokenResponse.kt
+++ app/src/main/java/com/ibra/keytrackerapp/common/token/domain/model/TokenResponse.kt
@@ -1,9 +1,11 @@
 package com.ibra.keytrackerapp.common.token.domain.model
 
+import com.ibra.keytrackerapp.key_requests.domain.enums.UserRole
 import kotlinx.serialization.Serializable
 
 
 @Serializable
 data class TokenResponse(
-    val token: String
+    val token: String,
+    val role: UserRole
 )
